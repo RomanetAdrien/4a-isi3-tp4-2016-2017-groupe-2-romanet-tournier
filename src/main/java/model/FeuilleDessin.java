bd93aa5367662659176d25ae23ef634d98fa7dc6
RomanetAdrien <adrien.romanet@etu.univ-lyon1.fr>
2017-05-22 09:19:18 +0200

    Fonctions de construction de maps

--- src/main/java/model/FeuilleDessin.java
+++ src/main/java/model/FeuilleDessin.java
@@ -18,13 +18,47 @@ public class FeuilleDessin  {
         tortueCourante = 0;
         listeObstacle = new ArrayList<>();
         ObstacleBord bord = new ObstacleBord();
-        ObstacleCercle cercle = new ObstacleCercle(50, new Point(200, 192));
+        ObstacleCercle cercle = new ObstacleCercle(50, new Point(0, 0));
         listeObstacle.add(bord);
         listeObstacle.add(cercle);
         for (int i =0; i< 5; i++){
             listeObstacle.add(new ObstacleCercle());
             listeObstacle.add(new ObstacleRectangle());
         }
+    }
+
+    public void ajouterObstacles(){
+        listeObstacle.clear();
+        listeObstacle.add(new ObstacleBord());
+        for (int i =0; i< 5; i++){
+            listeObstacle.add(new ObstacleCercle());
+            listeObstacle.add(new ObstacleRectangle());
+        }
+    }
+
+    public void creerLabyrinthe(int hauteur, int largeur){
+
+        listeObstacle.clear();
+        listeObstacle.add(new ObstacleBord());
+        listeObstacle.add(new ObstacleRectangle(new Point(0,0),2*largeur, (int) (hauteur*0.20)));
+        listeObstacle.add(new ObstacleRectangle(new Point(0,0), (int) (largeur*0.30), hauteur*2));
+        listeObstacle.add(new ObstacleRectangle(new Point(2*largeur,0), (int) (largeur*0.30), hauteur*2));
+        listeObstacle.add(new ObstacleRectangle(new Point(0,2*hauteur), largeur*2, (int) (hauteur*0.20)));
+        listeObstacle.add(new ObstacleRectangle(new Point(0,hauteur), largeur, (int) (hauteur*0.20)));
+        listeObstacle.add(new ObstacleRectangle(new Point((int) (largeur*1.25),hauteur), largeur, (int) (hauteur*0.20)));
+        listeObstacle.add(new ObstacleRectangle(new Point((int) (largeur*1.25), (int) (hauteur*0.75)), (int) (largeur*0.25), (int) (hauteur*0.25)));
+        listeObstacle.add(new ObstacleRectangle(new Point(0, (int) (hauteur*1.40)), (int) (largeur*1.75), (int) (hauteur*0.25)));
+        listeObstacle.add(new ObstacleRectangle(new Point(largeur, (int) (hauteur*1.40)), (int) (largeur*0.25), (int) (hauteur*0.5)));
+
+    }
+
+    public void dessinerMickey(int hauteur, int largeur){
+        listeObstacle.clear();
+        listeObstacle.add(new ObstacleBord());
+        listeObstacle.add(new ObstacleCercle((int) (largeur*0.5),new Point((int)(largeur*0.25+0.5*largeur),(int) (0.75*hauteur))));
+        listeObstacle.add(new ObstacleCercle((int) (largeur*0.5),new Point((int)(largeur*1.75+0.5*largeur),(int) (0.75*hauteur))));
+        listeObstacle.add(new ObstacleCercle((int) (largeur*0.75),new Point((int)(largeur+0.5*largeur),(int) (hauteur*1.25))));
+
     }
 
     public ArrayList<Obstacle> getListeObstacle() {
